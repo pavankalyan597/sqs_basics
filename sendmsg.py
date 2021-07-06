a71c7621adacdc5537e818039dd6ccff362e2887
pavan <pavanpasupuleti5@gmail.com>
2021-07-06 15:27:42 +0530

    Update sendmsg.py

--- sendmsg.py
+++ sendmsg.py
@@ -1,7 +1,5 @@
 import boto3
 
-client = boto3.client('sqs',region_name='us-east-2',aws_access_key_id='AKIA2DRAHDISQGOABUUC',aws_secret_access_key="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")
-
 queue = client.get_queue_url(
     QueueName='MyFirstQueue')
 
